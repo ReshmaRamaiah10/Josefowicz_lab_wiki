68d92de4620ea014ce3ab3ebe9cba953a3ad7a12
Reshma Ramaiah <99098898+ReshmaRamaiah10@users.noreply.github.com>
2024-10-22 14:03:57 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,6 +39,20 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': True,
+}
+
+# Add GitHub integration
+html_context = {
+    "display_github": True,  # Integrate GitHub
+    "github_user": "ReshmaRamaiah10",  # Your GitHub username
+    "github_repo": "Josefowicz_lab_wiki",  # Your repo name
+    "github_version": "main",  # Branch name (e.g., main or master)
+    "conf_py_path": "/docs/source/",  # Path to your docs root
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
