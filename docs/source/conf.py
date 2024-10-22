7c7e202efd904655af2c8f88adb502c0c22af5fd
Reshma Ramaiah <99098898+ReshmaRamaiah10@users.noreply.github.com>
2024-10-22 00:21:50 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Josefowicz Lab'
+copyright = '2024, Reshma'
+author = 'Reshma Ramaiah'
 
 release = '0.1'
 version = '0.1.0'
